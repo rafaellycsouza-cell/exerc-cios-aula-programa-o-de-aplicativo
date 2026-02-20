9e92afeb7e41eb1619bf8cae8ac17146f2a29c5b
rafaellycsouza-cell <rafaelly_c_souza@estudante.sesisenai.org.br>
2026-02-19 22:11:34 -0300

    terceira aula

---
+++ modelos.py
@@ -0,0 +1,29 @@
+'''
+
+Nome: Rafaelly Cristini
+
+Classe: ClassePedido
+
+Atributos:
+numero_pedido
+endereço
+nome
+valor_pedido
+possui_desconto
+
+Comportamentos:
+
+O “número_pedido” serve para armazenar os clientes que pediram 
+por ordem através dos números (pedido 01, pedido 02…). O “endereço” 
+serve para ter a localização da qual o pedido será entregue. O “nome” 
+guarda o nome do respectivo cliente. o “valor_pedido” guarda
+o valor que o pedido custou. “possui_desconto” armazena o desconto
+do cliente caso esteja em dia de promoção na loja.
+
+
+
+
+'''
+
+
+
